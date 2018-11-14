6d708d0f70b3f31b27287e8fee76448b0179b1e5
Tommy Siu <tommysiu0@hotmail.com>
2018-11-14 14:28:23 +0800

    v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dintaifung',
+    address:      'Jingan Kerry Center',
+    category:     'chinese'
+  },
+  {
+    name:         'Bella Napoli',
+    address:      'Nanhui road',
+    category:     'italian'
+  },
+  {
+    name:         'Yo Sushi',
+    address:      'Landan',
+    category:     'japanese'
+  },
+  {
+    name:         'La Cabane',
+    address:      'fumin road',
+    category:     'french'
+  },
+  {
+    name:         'Cafe Des Stagiere',
+    address:      'Found 158',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
